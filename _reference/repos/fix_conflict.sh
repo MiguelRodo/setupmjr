56fbbe25324aa390b516d7d6a064f2777979e5e8
Miguel Rodo <rdxmig002@myuct.ac.za>
2026-05-20 17:42:49 +0200

    Delete _reference directory, as no longer required

--- _reference/repos/fix_conflict.sh
+++
@@ -1,3 +0,0 @@
-sed -i 's/<<<<<<< Updated upstream//' tests/test-python-wrappers.py
-sed -i 's/=======//' tests/test-python-wrappers.py
-sed -i 's/>>>>>>> Stashed changes//' tests/test-python-wrappers.py
--- _reference/repos/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/MiguelRodo/repos/v2
-
-go 1.22
-
-require golang.org/x/term v0.20.0
-
-require golang.org/x/sys v0.20.0 // indirect
