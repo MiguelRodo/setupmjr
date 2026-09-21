3b0876408f3f1e8cc92223a56ac2ebcfc0eda3bb
Miguel Rodo <miguel.rodo@outlook.com>
2026-09-21 07:46:21 +0200

    Remove Python wrapper

--- setupmjr.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-
-def run(*args: str, check: bool = True, **kwargs):
-    """Run the installed setupmjr CLI."""
-    return subprocess.run(("setupmjr", *args), check=check, **kwargs)
